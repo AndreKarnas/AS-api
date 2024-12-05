849b315fb29854575d5485ea9e1cd6c23131e5a3
Andre Karnas <andreramoskarnas@gmail.com>
2024-12-05 19:51:47 -0300

    as

---
+++ Repositories/IPedidoRepository.cs
@@ -0,0 +1,13 @@
+public interface IPedidoRepository
+{
+
+    public List<Pedido> GetAll();
+
+    public Pedido Get(int id);
+
+    public void Post(Pedido pedido);
+
+    public Pedido Put(int id, Pedido pedidoAtualizado);
+
+    public Pedido Delete(int id);
+}
